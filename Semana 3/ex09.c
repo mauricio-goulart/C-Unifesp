23e30ad88dbb274f0de178c2798e5c0086d9740d
scarlet-project <mauricioazevedogou@gmail.com>
2023-04-16 11:16:57 -0300

    Update ex09.c

--- Semana 3/ex09.c
+++ Semana 3/ex09.c
@@ -10,7 +10,7 @@ int main()
     printf("-------Convertido-------\n");
     printf("Convertido e Celsius = [%.2f]\n",5*((f-32)/9));
     printf("------------------------");
-    printf("\n\nDigite enter para encerrar...");
+    printf("\n\nDigite um numero e enter para encerrar...");
     scanf("%i",enc);
 
     return 0;
