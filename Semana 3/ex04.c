3d7377a19a23d33254c1e25dc27381caa690a575
scarlet-project <mauricioazevedogou@gmail.com>
2023-04-15 23:30:40 -0300

    Update ex04.c

--- Semana 3/ex04.c
+++ Semana 3/ex04.c
@@ -3,8 +3,6 @@
 int main()
 {
     float n1,n2,n3,n4,media;
-    media = (n1+n2+n3+n4)/4;
-
     printf("----Escola----\n");
     printf("1*Nota = ");
     scanf("%f",&n1);
@@ -14,7 +12,10 @@ int main()
     scanf("%f",&n3);
     printf("4*Nota = ");
     scanf("%f",&n4);
-    
+    media = (n1+n2+n3+n4)/4;
+    printf("----Media----\n");
+    printf("Media = [%.2f]\n",media);
+        
 
 
 
