e5fecf9e0280b48bd3f18e8030a51e60434be5e4
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-02 21:06:05 -0300

    Update ex01.c

---
+++ Semana 5/ex01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2;
+    printf("Digite um numero: ");
+    scanf("%i",&n1);
+    printf("Digite outro numero: ");
+    scanf("%i",&n2);
+    printf("-----------------------");
+    if (n1 > n2){
+        printf("\nMaior = [%i]",n1);
+        printf("\nMenor = [%i]",n2);
+    }
+
+    else{
+        printf("\nMaior = [%i]",n2);
+        printf("\nMenor = [%i]",n1);
+
+    }
+
+    printf("\n-----------------------");
+
+
+
+
+
+
+}
