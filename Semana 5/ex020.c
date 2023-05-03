0cc155cf01b56302c5944ae9d03c7d446c3a3071
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-03 13:37:37 -0300

    Update ex020.c

--- Semana 5/ex020.c
+++ Semana 5/ex020.c
@@ -2,13 +2,18 @@
 
 int main()
 {
-    int n1,fat,c,cond;
+    int n1,fat,c;
+    char resposta;
+
+    do
+   {
+
     printf("----Fatorial----");
     printf("\nDigite um numero: ");
     scanf("%i",&n1);
     printf("----------------\n");
 
-    while (n1 > 16 || n1 <0)
+    while (n1 < 0 || n1 > 16)
     {
         printf("[ERRO]\n[Digite um numero positivo e menor que 16]");
         printf("\nDigite um numero: ");
@@ -17,9 +22,8 @@ int main()
 
     fat = n1;
     c = 1;
-    cond = 1;   
-    
-     while (fat>0)
+
+    while (fat > 0)
     {
         printf("%i",fat);
         if (fat == 1)
@@ -31,20 +35,30 @@ int main()
             printf("x");
         }
         
+
+
         c = c * fat;
         fat = fat - 1;
-
-        
-        
-        
     }
 
+    printf("%i",c);
+
+    printf("\n----------------");
+    printf("\nDeseja calcular outro fatorial? (s/n): ");
+    scanf(" %c", &resposta);
     
     
+
+   
+   
     
-    
+   } while (resposta == 's');
+
+   printf("\nFim do programa");
+   
         
     
+   
    
 
 
