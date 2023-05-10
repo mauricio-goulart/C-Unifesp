39d47cc882e8348afb5cd221ad25be70ca2f72b4
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-10 20:39:53 -0300

    Create ex035.c

---
+++ Semana 6/ex035.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n);
+
+    printf("Numeros primos entre 1 e %d:\n", n);
+    for (i = 2; i <= n; i++) {
+        int eh_primo = 1;
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                eh_primo = 0;
+                break;
+            }
+        }
+        if (eh_primo) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
