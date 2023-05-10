c1cd1640fe6ab824b9091417abae473995d56e23
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-10 20:34:38 -0300

    Create ex032.c

---
+++ Semana 6/ex032.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,fat,c;
+
+    printf("----Fatorial----");
+    printf("\nDigite um numero: ");
+    scanf("%i",&n1);
+    printf("----------------\n");
+    printf("Calculando o fatorial de %i! = ",n1);
+
+    fat = n1;
+    c = 1;
+
+    while (fat>0)
+    {
+        printf("%i",fat);
+        if (fat == 1)
+        {
+            printf("=");
+        }
+        else
+        {
+            printf(".");
+        }
+        
+        c = c * fat;
+        fat = fat - 1;
+        
+        
+    }
+    printf("%i",c);
+
+
+}
