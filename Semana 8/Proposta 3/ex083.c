ad842150007815c819a6cf643097c46a832af48b
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-30 18:15:55 -0300

    Create ex083.c

---
+++ Semana 8/Proposta 3/ex083.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int v[20];
+
+    printf("----Vetor----\n");
+
+    for (int c = 0; c < 20; c++)
+    {
+        printf("%i*Valor\n",c + 1);
+        printf("Numero: ");
+        scanf("%i",&v[c]);
+        printf("-------------\n");
+    }
+
+    printf("Ordem inversa do vetor: ");
+
+    for (int c = 19; c >= 0; c--)
+    {
+        printf("%i",v[c]);
+    }
+    
+    
+}
