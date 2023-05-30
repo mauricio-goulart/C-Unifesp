527d9f0a23739db08d0831972604dee654445c56
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-30 17:34:13 -0300

    Update ex080.c

--- Semana 8/Proposta 3/ex080.c
+++ Semana 8/Proposta 3/ex080.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 
+#define COR_VERMELHO "\x1b[31m"
+#define COR_RESET "\x1b[0m"
+
+
+
 int main()
 {
-    float v[3];
+    float v[3], maior = 0;
+    int p = 0;
     
     printf("-----Contador-----\n");
     for(int c = 0; c < 3; c++)
     {
-        printf("%i*Posição\n",c + 1);
+        printf("%i*Posicao\n",c + 1);
         printf("Valor: ");
         scanf("%f",&v[c]);
         printf("------------------\n");
         
         while(v[c] < 0)
         {
-            printf()
+            printf(COR_VERMELHO"[ERROR]\n"COR_RESET"[DIGITE NUMEROS POSITIVOS!]\n");
+            printf("Valor: ");
+            scanf("%f",&v[c]);
         }
-        
+
+        if (v[c] > maior)
+    {
+        maior = v[c];
+        p = c;
     }
+    }
+
+    printf("Maior numero = [%f]\n",maior);
+    printf("Posicao vetor = [%i]",p);
+
+
+    
+    
 
     return 0;
 }
