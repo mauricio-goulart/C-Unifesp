24d09b245c7f0b28584cc17f2b6643c41dc034b4
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-30 17:06:51 -0300

    Create ex080.c

---
+++ Semana 8/Proposta 3/ex080.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    float v[3];
+    
+    printf("-----Contador-----\n");
+    for(int c = 0; c < 3; c++)
+    {
+        printf("%i*Posição\n",c + 1);
+        printf("Valor: ");
+        scanf("%f",&v[c]);
+        printf("------------------\n");
+        
+        while(v[c] < 0)
+        {
+            printf()
+        }
+        
+    }
+
+    return 0;
+}
