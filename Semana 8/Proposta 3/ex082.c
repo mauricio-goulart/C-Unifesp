e17573db1a43f72483a6b1d9d78893489f2d3c08
scarlet-project <mauricioazevedogou@gmail.com>
2023-05-30 18:04:07 -0300

    Update ex082.c

--- Semana 8/Proposta 3/ex082.c
+++ Semana 8/Proposta 3/ex082.c
@@ -2,5 +2,35 @@
 
 int main()
 {
+    float A[10],B[10];
+    int x;
+
+    printf("-----Vetores-----\n");
+
+    printf("Digite o valor do termo X: ");
+    scanf("%i",&x);
+    printf("-----------------\n");
+
+
+
+    for (int c = 0; c < 10; c++)
+    {
+        printf("%i*Valor",c + 1);
+        printf("\nNumero: ");
+        scanf("%f",&A[c]);
+        printf("-----------------\n");
+    }
+
+    for (int c = 0; c < 10; c++)
+    {
+        B[c] = A[c] * x;
+    }
+
+    for (int c = 0; c < 10; c++)
+    {
+        printf(" %.f",B[c]);
+    }
+    
+    
     
 }
