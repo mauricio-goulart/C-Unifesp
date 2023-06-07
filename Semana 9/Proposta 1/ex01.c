1bd91f2634c4c9f6f462a4bcd6007ad19de1dc63
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-06-07 19:26:41 -0300

    Update ex01.c

--- Semana 9/Proposta 1/ex01.c
+++ Semana 9/Proposta 1/ex01.c
@@ -2,12 +2,22 @@
 
 int main()
 {
-    int k;
-    
-    do
+    int matriz[4][5];
+
+    printf("Preencha o vetor\n");
+    printf("--------------------\n");
+
+    for ( int l = 0; l < 4; l++)
     {
-        /* code */
-    } while (/* condition */);
+        for (int c = 0; c < 5; c++)
+        {
+            printf("Preencha:[%i][%i]", l,c);
+        }
+        
+        printf("\n");
+    }
+    
+    
     
     
 }
