d561ba42866ec4569bd679a2f12d45fb800bdf6b
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-07-03 18:45:54 -0300

    Update semana 6/7

--- Semana 6/Proposta 1/ex026.c
+++ Semana 6/Proposta 1/ex026.c
@@ -1,3 +1,6 @@
+/*
+Numa eleição existem três candidatos. Faça um programa que peça o número total de eleitores. Peça para cada eleitor votar e ao final mostrar o número de votos de cada candidato.
+*/
 #include <stdio.h>
 
 int main()
