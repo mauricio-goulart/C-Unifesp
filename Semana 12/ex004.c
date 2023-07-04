49ce328d133d168aa09f18d4f408d85af03fd254
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-07-03 21:10:57 -0300

    Create ex004.c

---
+++ Semana 12/ex004.c
@@ -0,0 +1,42 @@
+/*
+Escreva uma função que receba um número inteiro e retorne o inverso desse número. Por exemplo,
+se a função receber 123456 deve retornar 654321.
+*/
+#include <stdio.h>
+
+void Imprimir_menu()
+{
+    printf("-------------------------------------------\n");
+    printf("\tInverter String\n");
+    printf("-------------------------------------------\n");
+}
+
+char Inverter()
+{
+    char n1[30];
+    printf("Digite um numero: ");
+    scanf("%s", n1);
+
+    int l = strlen(n1);
+
+    if (n1[strlen(n1)-1] == '\n')
+    {
+        n1[strlen(n1) - 1] = '\0';
+    }
+    
+
+    for (int c = l; c >= 0; c--)
+    {
+        printf("%c",n1[c]);
+    }
+    
+
+}
+
+int main()
+{
+    Imprimir_menu();
+
+    Inverter();
+
+}
