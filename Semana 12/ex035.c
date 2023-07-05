94c5de989e9f250bf0329e2ae8d695751eb0b37b
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-07-05 20:47:46 -0300

    Update ex035.c

--- Semana 12/ex035.c
+++ Semana 12/ex035.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#define MAX 30
+
+void Imprimir_menu()
+{
+    printf("--------------------------\n");
+    printf("\tFUNCOES\n");
+    printf("--------------------------\n");   
+}
+
+int Strlen(char a[])
+{
+    int cont = 0;
+
+    for (int c = 0; c <= MAX; c++)
+    {
+        if (a[c] == '\0')
+        {
+            break;
+        }
+        
+        cont = cont + 1;
+    }
+    return cont;
+}
 
 int main()
 {
-    
+    char nome[MAX];
+
+    Imprimir_menu();
+
+    printf("Digite uma palavra: ");
+    scanf("%s", nome);
+
+    printf("%i", Strlen(nome));
+
 }
