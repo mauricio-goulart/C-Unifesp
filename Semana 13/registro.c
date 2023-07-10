8d414c7f9d8e569ae7cfa70af2b404e04a1010b6
Mauricio Azevedo Goulart <mauricioazevedogolf@gmail.com>
2023-07-10 15:20:24 -0300

    Update registro.c

--- Semana 13/registro.c
+++ Semana 13/registro.c
@@ -29,19 +29,58 @@ void Imprimir_menu()
 void Inserir_aluno(struct AlunosLP TurmaLP[], int n)
 {
     printf("Nome: ");
-    fgets(TurmaLP[n].nome, sizeof(TurmaLP[n].nome), stdin);
+    fgets(TurmaLP[n].nome,sizeof(TurmaLP[n].nome),stdin);
+    TurmaLP[n].nome[strlen(TurmaLP[n].nome) - 1] = '\0';
+
     printf("Ra: ");
-    scanf("%i", &TurmaLP[n].ra);
+    scanf("%d", &TurmaLP[n].ra);
+    getchar();
+
     printf("Frequencia: ");
-    scanf("%i", &TurmaLP[n].freq);
+    scanf("%d", &TurmaLP[n].freq);
+    getchar();
 
     for (int c = 0; c < 3; c++)
     {
-        printf("[%i] Nota: ",c);
-        scanf("%f", &TurmaLP[n].nota[c]);
+        printf("[%i] Nota: ",c + 1);
+        scanf(" %f", &TurmaLP[n].nota[c]);
+        getchar();
     }
     
 }
+
+int Consultar_aluno(struct AlunosLP TurmaLP[], int a)
+{
+    int ra;
+
+    printf("Digite o RA do aluno: ");
+    scanf("%i", &ra);
+    getchar();
+
+    for (int c = 0; c < a; c++)
+    {
+        if (TurmaLP[c].ra == ra)
+        {
+            return c;
+        }
+        
+    }
+    return -1;
+}
+
+void Mostrar_aluno(struct AlunosLP TurmaLP[], int c )
+{
+    printf("Nome: [%s]\n", TurmaLP[c].nome);
+    printf("Ra: [%d]\n",TurmaLP[c].ra);
+    printf("Frequencia: []\n",TurmaLP[c].freq);
+
+    for (int i = 0; i < 3; i++)
+    {
+        printf("[%i] Nota = [%d]\n", c + 1, TurmaLP[c].nota[i]);
+    }
+    
+}
+
 int main()
 {
     struct AlunosLP TurmaLP[5];
@@ -55,23 +94,49 @@ int main()
         Imprimir_menu();
         
         scanf("%i", &op);
+        getchar();
 
         switch (op)
         {
         case 1:
-            if (a == 50)
+            if (a == 5)
             {
                 printf("Turma já preenchida");
             }
             else
+            {   
+               
+                
+                if (Consultar_aluno(TurmaLP, a) == -1)
+                {
+                    Inserir_aluno(TurmaLP, a);
+                    a++;
+                }
+                else
+                {
+                    printf("ALuno ja matriculado");
+                }
+                
+             break; 
+            }
+            
+        case 2:
+            if (a == 0)
             {
-                Inserir_aluno(TurmaLP, a);
-                a++;
+                printf("Turma Vazia");
+            }
+            else
+            {
+                int aluno;
+
+                aluno = Consultar_aluno(TurmaLP, a);
+
+                Mostrar_aluno(TurmaLP, aluno); 
+                
+
             }
             
         
-        default:
-            break;
         }
 
     } while (op != 5);
